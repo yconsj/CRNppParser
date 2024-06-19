c87f8cbe1523f78a0e33ad3702844bbe76143a14
simon <yconsj@gmail.com>
2024-06-19 11:51:12 +0200

    lib, added parser

---
+++ CRNppInterpreter/Parser.fs
@@ -0,0 +1,178 @@
+﻿namespace Interperter
+
+module Parser =
+    open FParsec
+    type Species = string
+    type Number = float
+
+    type ModuleS =
+        | LD of Species * Species
+        | ADD of Species * Species * Species
+        | SUB of Species * Species * Species
+        | MUL of Species * Species * Species
+        | DIV of Species * Species * Species
+        | SQRT of Species * Species
+        | CMP of Species * Species
+
+
+    type CommandS =
+        | Module of ModuleS
+        | Conditional of ConditionalS
+
+    and ConditionalS =
+        | IfGT of CommandSList
+        | IfGE of CommandSList
+        | IfEQ of CommandSList
+        | IfLT of CommandSList
+        | IfLE of CommandSList
+
+    and CommandSList = CommandList of CommandS list
+
+    type StepS = Step of CommandSList
+
+    type ConcS = Conc of Species * Number
+
+    type RootS =
+        | RootStep of StepS
+        | RootConc of ConcS
+
+
+    type RootSList = RootList of RootS list
+
+
+    type Crn = Crn of RootSList
+
+
+    let (pCrn, pCrnRef) = createParserForwardedToRef<Crn, unit> ()
+
+    // Forward declaration of pCommandS and pCommandSList
+
+    let pCommandSList, pCommandSListRef = createParserForwardedToRef ()
+
+    let token p = p .>> spaces
+    let symbol s = token (pstring s)
+
+    let ident =
+        let charOrDigit c = isLetter c || isDigit c
+        spaces >>. token (many1Satisfy2L isLetter charOrDigit "identifier")
+
+
+    let betweenBrackets p = between (pstring "[") (pstring "]") p
+
+    let betweenCurlyBrackets p =
+        between (pstring "{") (spaces >>. pstring "}") p
+
+    let commaSeparated2 p1 p2 = tuple2 p1 (pchar ',' >>. spaces >>. p2)
+
+    let commaSeparated3 p1 p2 p3 =
+        tuple3 p1 (pchar ',' >>. spaces >>. p2) (pchar ',' >>. spaces >>. p3)
+
+    let pLd =
+        pstring "ld" >>. spaces >>. betweenBrackets (commaSeparated2 ident ident)
+        |>> fun (s1, s2) -> LD(s1, s2)
+
+    let pAdd =
+        pstring "add" >>. spaces >>. betweenBrackets (commaSeparated3 ident ident ident)
+        |>> fun (s1, s2, s3) -> ADD(s1, s2, s3)
+
+    let pSub =
+        pstring "sub" >>. spaces >>. betweenBrackets (commaSeparated3 ident ident ident)
+        |>> fun (s1, s2, s3) -> SUB(s1, s2, s3)
+
+    let pMul =
+        pstring "mul" >>. spaces >>. betweenBrackets (commaSeparated3 ident ident ident)
+        |>> fun (s1, s2, s3) -> MUL(s1, s2, s3)
+
+    let pDiv =
+        pstring "div" >>. spaces >>. betweenBrackets (commaSeparated3 ident ident ident)
+        |>> fun (s1, s2, s3) -> DIV(s1, s2, s3)
+
+    let pSqrt =
+        pstring "sqrt" >>. spaces >>. betweenBrackets (commaSeparated2 ident ident)
+        |>> fun (s1, s2) -> SQRT(s1, s2)
+
+    let pCmp =
+        pstring "cmp" >>. spaces >>. betweenBrackets (commaSeparated2 ident ident)
+        |>> fun (s1, s2) -> CMP(s1, s2)
+
+
+    let pModuleS =
+        spaces >>. choice [ pCmp; pSub; pAdd; pLd; pSqrt; pDiv; pMul ]
+        |>> fun (arith) -> arith
+
+
+
+
+    let pIfGT =
+        spaces >>. pstring "ifGT" >>. spaces >>. betweenBrackets (pCommandSList)
+        |>> fun x -> IfGT(x)
+
+    let pIfGE =
+        spaces >>. pstring "ifGE" >>. spaces >>. betweenBrackets (pCommandSList)
+        |>> fun x -> IfGE(x)
+
+    let pIfEQ =
+        spaces >>. pstring "ifEQ" >>. spaces >>. betweenBrackets (pCommandSList)
+        |>> fun x -> IfEQ(x)
+
+    let pIfLT =
+        spaces >>. pstring "ifLT" >>. spaces >>. betweenBrackets (pCommandSList)
+        |>> fun x -> IfLT(x)
+
+    let pIfLE =
+        spaces >>. pstring "ifLE" >>. spaces >>. betweenBrackets (pCommandSList)
+        |>> fun x -> IfLE(x)
+
+    let pConditionalS =
+        choice
+            [ pIfGT |>> fun x -> x
+              pIfEQ |>> fun x -> x
+              pIfLT |>> fun x -> x
+              pIfLE |>> fun x -> x ]
+
+    let pCommandS =
+        spaces
+        >>. choice
+            [ pConditionalS .>> spaces |>> fun cond -> Conditional(cond)
+              pModuleS .>> spaces |>> fun (mod) -> Module (mod) ]
+
+    let pCommandSListImpl =
+        sepBy1 pCommandS (spaces >>. skipString "," >>. spaces)
+        |>> fun l -> CommandList(l)
+
+    pCommandSListRef.Value <- pCommandSListImpl
+
+
+
+    let pStepS =
+        (pstring "step") >>. spaces >>. betweenBrackets (pCommandSList)
+        |>> fun x -> Step(x)
+
+    let pConcS =
+        (pstring "conc")
+        >>. spaces
+        >>. betweenBrackets ((spaces >>. ident .>> spaces .>> pchar ',') .>>. (spaces >>. pfloat .>> spaces))
+        |>> fun (species, number) -> Conc(species, number)
+
+    let pRootS =
+        spaces
+        >>. choice [ (pConcS |>> fun x -> RootConc(x)); (pStepS |>> fun x -> RootStep(x)) ]
+        .>> spaces
+
+    let pRootSList =
+
+        sepBy1 pRootS (skipString ",") |>> fun l -> RootList(l)
+
+
+    pCrnRef.Value <-
+        parse {
+            let! x =
+                spaces
+                >>. pstring "crn"
+                >>. spaces
+                >>. pstring "="
+                >>. spaces
+                >>. betweenCurlyBrackets (pRootSList)
+
+            return Crn(x)
+        }
