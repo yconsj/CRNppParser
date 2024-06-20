c8d763d9013824d946a64792607630131ffbef9d
SimonJ <yconsj@gmail.com>
2024-06-20 13:10:17 +0200

    merge fix

--- Interpreter.fsx
+++ Interpreter.fsx
@@ -1,5 +1,8 @@
 #r "CRNppInterpreter/Library/net7.0/CRNppInterpreter.dll"
+#r "nuget: Plotly.NET, 4.0.0"
 
+open Plotly.NET
+open Plotly.NET.LayoutObjects // this namespace contains all object abstractions for layout styling
 open Interperter.Parser
 
 
@@ -21,6 +24,13 @@ let ExecuteModule m (state: Map<Species, Number>) =
     | SUB(s1, s2, s3) ->
         let (A, B, C) = getThreeSpecies state s1 s2 s3
         Map.add s3 (A - B - C) state
+    | CMP(s1,s2) -> 
+        let (v1,v2) = (Map.find s1 state, Map.find s2 state)
+        match (v1,v2) with
+        | v1,v2 when v1 > v2 -> Map.add "_flag" 3 state
+        | v1, v2 when v1 = v2 -> Map.add "_flag" 2 state
+        | v1, v2 when v1 < v2 -> Map.add "_flag" 1 state
+        | _ -> failwith "compare error between"
     | _ -> state
 
 let ExecuteConditional c (state: Map<Species, Number>) =
@@ -39,33 +49,74 @@ let rec steps state stps =
     match stps with
     | h :: [] -> step state h
     | h :: t -> steps (step state h) t
-
+    | _ -> failwith "error no steps"
 let rec getSteps rootList =
     match rootList with
     | Conc(x) :: t -> getSteps t
+    | Step(x) :: [] -> [x]
     | Step(x) :: t -> x :: getSteps t
+    | [] -> []
     | _ -> failwith "Error getsteps"
 
-let intializeConcentrations rootList =
-    let rec intializeConcentrations' rootList' state =
-        match rootList with
-        | Conc(species, number) :: [] -> (Map.add species number state)
-        | Conc(species, number) :: t -> intializeConcentrations' t (Map.add species number state)
-        | Step(x) :: t -> intializeConcentrations' t state
-        | _ -> failwith "Error Intilize concentrations"
 
-    intializeConcentrations' rootList Map.empty
+let rec helper cmdl state =
+    match cmdl with
+    | Module(MUL(a,b,c))::t
+    | Module(DIV(a,b,c))::t
+    | Module(SUB(a,b,c))::t
+    | Module(ADD(a,b,c))::t -> 
+        let newState = Map.add c 0.0 (Map.add b 0.0(Map.add a 0 state))
+        helper t newState
+    | Module(LD(a,b))::t
+    | Module(SQRT(a,b))::t ->
+        let newState = (Map.add b 0.0(Map.add a 0.0 state))
+        helper t newState
+    | Module(CMP(a,b))::t -> 
+        let newState = Map.add "_flag" 0.0 (Map.add b 0.0(Map.add a 0.0 state))
+        helper t newState
+    | Conditional(IfGE(l))::t
+    | Conditional(IfEQ(l))::t
+    | Conditional(IfGE(l))::t
+    | Conditional(IfGT(l))::t -> let newState = helper l state
+                                 helper t newState
+    | [] -> state
+let intializeVariables rootList  =
+    let rec intializeVariables' rootList' state =
+        match rootList' with 
+        | Conc(_) :: t  -> intializeVariables' t state
+        | Step(x) :: t  -> intializeVariables' t (helper x state)
+        | [] -> state
+
+    intializeVariables' rootList Map.empty
+
+let rec intializeConcentrations rootList state =
+    match rootList with
+    | Conc(species, number) :: t    -> intializeConcentrations t (Map.add species number state)
+    | Step(x) :: t                  -> intializeConcentrations t state
+    | [] -> state
+    | _ -> failwith "Error Intilize concentrations"
+
 
 let CrnProgram (programTree: Crn) =
 
-
-
     let (CRN rl) = programTree
     let stps = getSteps rl
-    let (s0: Map<Species, Number>) = intializeConcentrations rl
-
+    let variables = intializeVariables rl
+    let (s0: Map<Species, Number>) = intializeConcentrations rl variables
+    printfn "%A \n" stps
+    printfn "%A \n" s0
     Seq.unfold
         (fun state ->
             let newState = steps state stps
             Some(newState, newState))
         s0
+
+
+let testProgramTree=  CRN[Conc("a",2.0); Conc("b",1.0);Step[Module (ADD("a","b","c"))]]
+
+
+
+printf "%A" (CrnProgram testProgramTree)
+
+
+
--- TestParser.fsx
+++ TestParser.fsx
@@ -5,8 +5,12 @@ type Species = string
 type Number = float
 
 type ModuleS =
-    | ADD of Species * Species * Species 
+    | LD of Species * Species
+    | ADD of Species * Species * Species
     | SUB of Species * Species * Species
+    | MUL of Species * Species * Species
+    | DIV of Species * Species * Species
+    | SQRT of Species * Species
     | CMP of Species * Species
 
 
@@ -14,28 +18,35 @@ type CommandS =
     | Module of ModuleS 
     | Conditional of ConditionalS
 
-and ConditionalS = IfGT of CommandSList
+and ConditionalS =
+    | IfGT of CommandSList
+    | IfGE of CommandSList
+    | IfEQ of CommandSList
+    | IfLT of CommandSList
+    | IfLE of CommandSList
 
-and CommandSList = CommandList of CommandS list
+and CommandSList = CommandS list
 
-type StepS = Step of CommandSList
+type StepS = CommandSList
 
-type ConcS = Conc of Species * Number
+type ConcS = Species * Number
 
 type RootS =
-    | RootStep of StepS
-    | RootConc of ConcS
+    | Step of StepS
+    | Conc of ConcS
 
 
-type RootSList = RootList of RootS list
+type RootSList = RootS list
 
 
-type Crn = Crn of RootSList
+type Crn = CRN of RootSList
 
 
 let (pCrn, pCrnRef) = createParserForwardedToRef<Crn, unit> ()
 
+// Forward declaration of pCommandS and pCommandSList
 
+let pCommandSList, pCommandSListRef = createParserForwardedToRef ()
 
 let token p = p .>> spaces
 let symbol s = token (pstring s)
@@ -48,82 +59,141 @@ let ident =
 let betweenBrackets p = between (pstring "[") (pstring "]") p
 
 let betweenCurlyBrackets p =
-    between (pstring "{" >>. spaces) (spaces >>. pstring "}") p
+    between (pstring "{") (spaces >>. pstring "}") p
 
 let commaSeparated2 p1 p2 = tuple2 p1 (pchar ',' >>. spaces >>. p2)
 
 let commaSeparated3 p1 p2 p3 =
     tuple3 p1 (pchar ',' >>. spaces >>. p2) (pchar ',' >>. spaces >>. p3)
 
+let pLd =
+    pstring "ld" >>. spaces >>. betweenBrackets (commaSeparated2 ident ident)
+    |>> fun (s1, s2) -> LD(s1, s2)
+
 let pAdd =
-    spaces
-    >>. pstring "add"
-    >>. spaces
-    >>. betweenBrackets (commaSeparated3 ident ident ident)
+    pstring "add" >>. spaces >>. betweenBrackets (commaSeparated3 ident ident ident)
     |>> fun (s1, s2, s3) -> ADD(s1, s2, s3)
 
 let pSub =
-    spaces
-    >>. pstring "sub"
-    >>. spaces
-    >>. betweenBrackets (commaSeparated3 ident ident ident)
+    pstring "sub" >>. spaces >>. betweenBrackets (commaSeparated3 ident ident ident)
     |>> fun (s1, s2, s3) -> SUB(s1, s2, s3)
 
+let pMul =
+    pstring "mul" >>. spaces >>. betweenBrackets (commaSeparated3 ident ident ident)
+    |>> fun (s1, s2, s3) -> MUL(s1, s2, s3)
+
+let pDiv =
+    pstring "div" >>. spaces >>. betweenBrackets (commaSeparated3 ident ident ident)
+    |>> fun (s1, s2, s3) -> DIV(s1, s2, s3)
+
+let pSqrt =
+    pstring "sqrt" >>. spaces >>. betweenBrackets (commaSeparated2 ident ident)
+    |>> fun (s1, s2) -> SQRT(s1, s2)
+
 let pCmp =
-    spaces
-    >>. pstring "cmp"
-    >>. spaces
-    >>. betweenBrackets (commaSeparated2 ident ident)
+    pstring "cmp" >>. spaces >>. betweenBrackets (commaSeparated2 ident ident)
     |>> fun (s1, s2) -> CMP(s1, s2)
 
+
 let pModuleS =
-    choice [ attempt pCmp; attempt pSub; attempt pAdd ] |>> fun (arith) -> arith
+    spaces >>. choice [ pCmp; pSub; pAdd; pLd; pSqrt; pDiv; pMul ]
+    |>> fun (arith) -> arith
+
+
+
+
+let pIfGT =
+    spaces >>. pstring "ifGT" >>. spaces >>. betweenBrackets (pCommandSList)
+    |>> fun x -> IfGT(x)
+
+let pIfGE =
+    spaces >>. pstring "ifGE" >>. spaces >>. betweenBrackets (pCommandSList)
+    |>> fun x -> IfGE(x)
+
+let pIfEQ =
+    spaces >>. pstring "ifEQ" >>. spaces >>. betweenBrackets (pCommandSList)
+    |>> fun x -> IfEQ(x)
+
+let pIfLT =
+    spaces >>. pstring "ifLT" >>. spaces >>. betweenBrackets (pCommandSList)
+    |>> fun x -> IfLT(x)
+
+let pIfLE =
+    spaces >>. pstring "ifLE" >>. spaces >>. betweenBrackets (pCommandSList)
+    |>> fun x -> IfLE(x)
+
+let pConditionalS =
+    choice
+        [ pIfGT |>> fun x -> x
+          pIfEQ |>> fun x -> x
+          pIfLT |>> fun x -> x
+          pIfLE |>> fun x -> x ]
 
 let pCommandS =
-    choice [ spaces >>. pModuleS .>> spaces ] |>> fun (mod) -> Module (mod)
+    spaces
+    >>. choice
+        [ pConditionalS .>> spaces |>> fun cond -> Conditional(cond)
+          pModuleS .>> spaces |>> fun (mod) -> Module (mod) ]
+
+let pCommandSListImpl =
+    sepBy1 pCommandS (spaces >>. skipString "," >>. spaces) |>> fun l -> l
+
+pCommandSListRef.Value <- pCommandSListImpl
 
 
-let pCommandSList =
-      sepBy1 pCommandS (spaces >>. skipString "," >>. spaces) |>> fun l -> CommandList(l)
 
 let pStepS =
-    (pstring "step") >>. betweenBrackets (pCommandSList) |>> fun x -> Step(x)
+    (pstring "step") >>. spaces >>. betweenBrackets (pCommandSList) |>> fun x -> x
 
 let pConcS =
     (pstring "conc")
+    >>. spaces
     >>. betweenBrackets ((spaces >>. ident .>> spaces .>> pchar ',') .>>. (spaces >>. pfloat .>> spaces))
-    |>> fun (species, number) -> Conc(species, number)
+    |>> fun (species, number) -> species, number
 
 let pRootS =
-    choice
-        [ attempt pConcS |>> fun x -> RootConc(x)
-          attempt pStepS |>> fun x -> RootStep(x) ]
+    spaces
+    >>. choice [ (pConcS |>> fun x -> Conc(x)); (pStepS |>> fun x -> Step(x)) ]
+    .>> spaces
 
 let pRootSList =
 
-    sepBy1 pRootS (spaces >>. skipString "," >>. spaces) |>> fun l -> RootList(l)
+    sepBy1 pRootS (skipString ",") |>> fun l -> l
 
 
 pCrnRef.Value <-
     parse {
         let! x =
-            pstring "crn"
+            spaces
+            >>. pstring "crn"
             >>. spaces
             >>. pstring "="
             >>. spaces
             >>. betweenCurlyBrackets (pRootSList)
 
-        return Crn(x)
+        return CRN(x)
     }
 
 let test p str =
     match run p str with
     | Success(result, _, _) -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
-let extract p str =
-    match run pCrn str with
-    | Success(result, _, _) -> result
-    | Failure(errorMsg, _, _) -> Crn(RootList([]))
 
+test pCrn " crn = { conc[A,2] , conc [ A, 2 ],step [ add [ A , B , C ] ] } "
 
-//test pCrn "crn = { conc[A,2],  conc[A,2], step[add[A,B,C]]} "
+let testProgram =
+    "
+            crn = {
+            conc[c,2 ], conc[ cInitial ,2 ],
+            conc[one ,1], conc[zero ,0],
+            step[
+            sub[c,one,cnext ],
+            cmp[c,zero]
+            ],
+            step[
+            ifGT[ ld [cnext ,c] ],
+             ifLE[ ld [ cInitial ,c] ]
+             ] }
+                "
+
+test pCrn testProgram
