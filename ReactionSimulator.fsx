3a5d5803ec1134e3c64e1c0e55af695cfab3ddf6
simon <yconsj@gmail.com>
2024-06-25 00:24:43 +0200

    idk merge

--- ReactionSimulator.fsx
+++ ReactionSimulator.fsx
@@ -1,3 +1,5 @@
+module Sim
+
 #r "CRNppInterpreter\\Library\\net7.0\\CRNppInterpreter.dll"
 #r "nuget: FParsec, 1.1.1"
 #r "nuget: Plotly.NET, 4.0.0"
@@ -17,15 +19,20 @@ open System
 
 // old simulator
 let odes (reactions: RxnS list) (species: Species list) (t: float) (concs: Vector<float>) =
-    let concDict = Map.ofList [ for i in 0 .. species.Length - 1 -> species.[i], concs.[i] ]
-    let countMatchesList el li = li |> (Seq.filter (fun x -> x=el) >> Seq.length)
+    let concDict =
+        Map.ofList [ for i in 0 .. species.Length - 1 -> species.[i], concs.[i] ]
+
+    let countMatchesList el li =
+        li |> (Seq.filter (fun x -> x = el) >> Seq.length)
+
     let netChange sp lhs rhs =
         let lhsCount = countMatchesList sp lhs
         let rhsCount = countMatchesList sp rhs
         double (rhsCount - lhsCount)
-    let reactantProduct (concs: State) (reactants: Expr) = 
+
+    let reactantProduct (concs: State) (reactants: Expr) =
         List.fold (fun acc r -> concs.[r] * acc) 1.0 reactants
-    
+
     let reactionChangeSpeciesFunc sp concs (reaction: RxnS) =
         match reaction with
         | (lhs, rhs, rate) -> rate * (netChange sp lhs rhs) * reactantProduct concs lhs
@@ -37,33 +44,41 @@ let odes (reactions: RxnS list) (species: Species list) (t: float) (concs: Vecto
 
 // Function to solve the ODEs for one time step
 let solveStep (initialConcs: Vector<float>) (reactions: RxnS list) (species: Species list) (timeStepSize: float) =
-    let odeFunc (t: float) (concs: Vector<float>) =
-        odes reactions species t concs
+    let odeFunc (t: float) (concs: Vector<float>) = odes reactions species t concs
     let resolution = 5
     RungeKutta.SecondOrder(initialConcs, 0.0, timeStepSize, resolution, odeFunc)
-    
+
 // Infinite sequence of states using ODE solver
 let reactionSimulator (initialConcs: State) (reactions: RxnS list) (timeStepSize: float) =
     let species = List.ofSeq initialConcs.Keys
-    let initialConcsVector = Vector<float>.Build.Dense([| for sp in species -> initialConcs.[sp] |])
+
+    let initialConcsVector =
+        Vector<float>.Build.Dense([| for sp in species -> initialConcs.[sp] |])
+
     let arrayToState (species: Species list) (concs: Vector<float>) =
-        let stateList = List.zip species (List.ofArray (concs.AsArray()))
-                        |> List.map (fun (sp, conc) -> sp, conc)
+        let stateList =
+            List.zip species (List.ofArray (concs.AsArray()))
+            |> List.map (fun (sp, conc) -> sp, conc)
+
         Map.ofList stateList
 
-    Seq.unfold (fun stateVector ->
-        let newStateArray = solveStep stateVector reactions species timeStepSize
-        let newStateVector = newStateArray.[newStateArray.Length - 1] // Take the last state in the array
-        Some(arrayToState species newStateVector, newStateVector)
-    ) initialConcsVector
+    Seq.unfold
+        (fun stateVector ->
+            let newStateArray = solveStep stateVector reactions species timeStepSize
+            let newStateVector = newStateArray.[newStateArray.Length - 1] // Take the last state in the array
+            Some(arrayToState species newStateVector, newStateVector))
+        initialConcsVector
 
-let reactionSimulatorPlot (initialConcs: State) (reactions: RxnS list) (timeStepSize : float) (timeSteps : int) =
+let reactionSimulatorPlot (initialConcs: State) (reactions: RxnS list) (timeStepSize: float) (timeSteps: int) =
     // timeResolution: how detailed the values are generated. value of >0
     // timeStep: Total number of time units to calculate the state. Value of >1
     // the total number of data points will be = timeStep / timeResolution
     let nDataPoints = int (float timeSteps / timeStepSize)
-    let yData : State seq = Seq.take nDataPoints (reactionSimulator initialConcs reactions timeStepSize)
-    let xData = [0.0 .. timeStepSize .. timeSteps]
+
+    let yData: State seq =
+        Seq.take nDataPoints (reactionSimulator initialConcs reactions timeStepSize)
+
+    let xData = [ 0.0 .. timeStepSize .. timeSteps ]
     smoothSimPlot xData yData
 
 
@@ -79,19 +94,18 @@ let reactionSimulatorPlot (initialConcs: State) (reactions: RxnS list) (timeStep
 // simulationPlot (fig1States) 1000
 
 
-let fig4OscConcs : State  = Map([("X_1",2);("X_2",0.01);("X_3",0.005);
-    ])
-let fig4OscReaction : RxnS list = [
-    RxnS(["X_1";"X_2"],["X_2";"X_2"],1.0);
-    RxnS(["X_2";"X_3"],["X_3";"X_3"],1.0);
-    RxnS(["X_3";"X_1"],["X_1";"X_1"],1.0);
-    ]
+let fig4OscConcs: State = Map([ ("X_1", 2); ("X_2", 0.01); ("X_3", 0.005) ])
+
+let fig4OscReaction: RxnS list =
+    [ RxnS([ "X_1"; "X_2" ], [ "X_2"; "X_2" ], 1.0)
+      RxnS([ "X_2"; "X_3" ], [ "X_3"; "X_3" ], 1.0)
+      RxnS([ "X_3"; "X_1" ], [ "X_1"; "X_1" ], 1.0) ]
 
 //let fig4States = (reactionSimulator fig4OscConcs fig4OscReaction 1)
 //simulationPlot (fig4States) 100
 
 
-reactionSimulatorPlot  fig4OscConcs fig4OscReaction 0.5 100
+//reactionSimulatorPlot fig4OscConcs fig4OscReaction 0.5 100
 
 /// step[]
 /// step[... cmp ...]
@@ -99,10 +113,17 @@ reactionSimulatorPlot  fig4OscConcs fig4OscReaction 0.5 100
 /// step[
 ///
 ///     ifGT[XgtY&clockspecies
-///     // XgtY & XltY  
+///     // XgtY & XltY
 ///     // [XgtY] * [XltY]
 ///  ]
 /// ]
-/// 
-/// // 4 * 3 clock species 
-
+///
+/// // 4 * 3 clock species
+/// // 4 * 3 clock species
+/// // 4 * 3 clock species
+/// // 4 * 3 clock species
+/// // 4 * 3 clock species
+/// // 4 * 3 clock species
+/// // 4 * 3 clock species
+/// // 4 * 3 clock species
+/// // 4 * 3 clock species
